337d9a277fb4f56c50f98d2578a17a4ec8957d2f
马思亮 <masiliang@Mac.lan>
2025-10-19 22:53:44 -0400

    Reset Assignment1 branch: keep only assignment1 folder

--- assignment2/helper_lib/__init__.py
+++
@@ -1,5 +0,0 @@
-from .data_loader import get_data_loader, get_cifar10_loaders
-from .model import get_model
-from .trainer import train_model
-from .evaluator import evaluate_model
-from .utils import save_model
